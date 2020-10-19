d7c8605e8eb672a1bb1f84c93705d3e6d89c9dff
YanyangChen <184536632@qq.com>
2020-10-19 16:45:12 +0800

    test

--- venv/cron_updateHK.py
+++ venv/cron_updateHK.py
@@ -1,5 +1,5 @@
 from StockObj import StockObj
-from datetime import *
+from datetime import * 
 
 with open('/home/Aaron/PyStocks/venv/tks/HKtks2') as f:
     lines = f.read().splitlines()
@@ -8,6 +8,6 @@ for line in lines:
     abc = StockObj("abc", str(line))
     # conn = abc.create_connection("/Users/chenyanyang/tst.db")
     conn = abc.create_connection("/home/Aaron/PyStocks/tst.db")
-    abc.update((datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d"), datetime.today().strftime("%Y-%m-%d"))
+    abc.update((datetime.today() - timedelta(days=3)).strftime("%Y-%m-%d"), datetime.today().strftime("%Y-%m-%d"))
 
 
--- venv/search.py
+++ venv/search.py
@@ -2,7 +2,7 @@ from StockObj import StockObj
 
 
 def search_up_days(days):
-        with open('../hkstk') as f:
+        with open('/home/Aaron/PyStocks/venv/tks/HKtks2') as f:
             lines = f.read().splitlines()
         hk = 0
         for line in lines:
@@ -15,48 +15,48 @@ def search_up_days(days):
                 hk = hk + 1
         print(str(hk) + " hk stocks met requirements")
 
-        with open('./tks/SZtks') as f:
-            lines = f.read().splitlines()
-        sz = 0
-        for line in lines:
-            abc = StockObj("abc", str(line)+".SZ")
-            # conn = abc.create_connection("/Users/chenyanyang/tst.db")
-            conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
+#        with open('./tks/SZtks') as f:
+#            lines = f.read().splitlines()
+#        sz = 0
+#        for line in lines:
+#            abc = StockObj("abc", str(line)+".SZ")
+#            # conn = abc.create_connection("/Users/chenyanyang/tst.db")
+#            conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
 
-            if abc.get_up(conn, days) is True:
-                print(str(abc.stock))
-                sz = sz + 1
-        print(str(sz) + " sz stocks met requirements")
+#            if abc.get_up(conn, days) is True:
+#                print(str(abc.stock))
+#                sz = sz + 1
+#        print(str(sz) + " sz stocks met requirements")
 
-        with open('./tks/SStks') as f:
-            lines = f.read().splitlines()
-        ss = 0
-        for line in lines:
-            abc = StockObj("abc", str(line)+".SS")
-            # conn = abc.create_connection("/Users/chenyanyang/tst.db")
-            conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
-
-            if abc.get_up(conn, days) is True:
-                print(str(abc.stock))
-                ss = ss + 1
-        print(str(ss) + " ss stocks met requirements")
-
-
-def search_up_days4USA(days):
-
-    with open('./tks/USAtks') as f:
-        lines = f.read().splitlines()
-        usa = 0
-    for line in lines:
-        abc = StockObj("abc", str(line))
-        # conn = abc.create_connection("/Users/chenyanyang/tst.db")
-        conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
-
-        if abc.get_upusa(conn, days) is True:
-            print(str(abc.stock))
-            usa = usa + 1
-    print(str(usa) + " usa stocks met requirements")
-
-
-search_up_days4USA(13)
-search_up_days(3)
+#        with open('./tks/SStks') as f:
+#            lines = f.read().splitlines()
+#        ss = 0
+#        for line in lines:
+#            abc = StockObj("abc", str(line)+".SS")
+#            # conn = abc.create_connection("/Users/chenyanyang/tst.db")
+#            conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
+#
+#            if abc.get_up(conn, days) is True:
+#                print(str(abc.stock))
+#                ss = ss + 1
+#        print(str(ss) + " ss stocks met requirements")
+#
+#
+#def search_up_days4USA(days):
+#
+#    with open('./tks/USAtks') as f:
+#        lines = f.read().splitlines()
+#        usa = 0
+#    for line in lines:
+#        abc = StockObj("abc", str(line))
+#        # conn = abc.create_connection("/Users/chenyanyang/tst.db")
+#        conn = abc.create_connection("/home/Aaron/PyStocks/venv/tst.db")
+#
+#        if abc.get_upusa(conn, days) is True:
+#            print(str(abc.stock))
+#            usa = usa + 1
+#    print(str(usa) + " usa stocks met requirements")
+#
+#
+#search_up_days4USA(13)
+search_up_days(1)
